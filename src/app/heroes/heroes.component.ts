7b1afbaa02764a5b0cbfc17bd1072d7c4fba8201
Kareen Castro <kareen.ketleen@gmail.com>
2023-12-04 20:31:01 -0400

    feat: add heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
-import {HEROES} from './mocks/Hero.mocks'
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
 
@@ -34,5 +33,16 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroesMock().subscribe(
       heroes => this.heroes = heroes
-  )}
+    )
+  }
+  
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
 }
